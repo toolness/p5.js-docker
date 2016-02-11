2822db262036140a8b6c32d7065063db47c26bec
Atul Varma <varmaa@gmail.com>
2016-02-11 06:58:10 -0500

    Always set HOME and setuid(). Fixes #5.

--- p5-website-bridge/entrypoint.js
+++ p5-website-bridge/entrypoint.js
@@ -6,17 +6,19 @@ var HOST_UID = fs.statSync('/var/p5.js').uid;
 var HOST_USER = process.env.HOST_USER || 'code_executor_user';
 
 if (HOST_UID !== process.getuid()) {
+  process.env['HOME'] = '/home/' + HOST_USER;
+
   if (!putil.successSync('id -u ' + HOST_USER)) {
     console.log("Configuring uid " + HOST_UID + " as user " +
                 HOST_USER + "...");
-    process.env['HOME'] = '/home/' + HOST_USER;
     putil.runSync(
       'groupadd code_executor_group && ' +
       'useradd -d ' + process.env['HOME'] + ' -m ' + HOST_USER + ' ' +
       '-g code_executor_group -u ' + HOST_UID
     );
-    process.setuid(HOST_UID);
   }
+
+  process.setuid(HOST_UID);
 }
 
 putil.spawnAndBindLifetimeTo(process.argv[2], process.argv.slice(3));
